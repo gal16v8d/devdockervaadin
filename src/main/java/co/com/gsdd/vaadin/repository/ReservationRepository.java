e300710321e8d75a9e76e32ce5943884dea50280
Alex Galvis <alex.galvis.sistemas@gmail.com>
2020-05-17 11:52:56 -0500

    Init repo

---
+++ src/main/java/co/com/gsdd/vaadin/repository/ReservationRepository.java
@@ -0,0 +1,21 @@
+package co.com.gsdd.vaadin.repository;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import co.com.gsdd.vaadin.model.Reservation;
+
+@RepositoryRestResource(collectionResourceRel = "reservations", path = "reservations")
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+	List<Reservation> findByReservationOwner(@Param("reservationOwner") String reservationOwner);
+
+	Collection<Reservation> findByReservationOwnerContainsIgnoreCase(String filterText);
+
+	Reservation findOne(Long id);
+
+}
